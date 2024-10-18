2f0666e8f7117a8b51006bf0973fcab938d9b1b2
beaudario <dariodouma@icloud.com>
2024-10-18 19:34:29 +0200

    Made changes to Home.vue, Changed rounded profile picture to hidden on lg and made it show to the right of paragraphs for responsiveness

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,33 +3,38 @@
     <div class="flex items-center">
       <h1 class="text-4xl pr-6 font-bold lg:text-6xl">Hi, i'm Dario</h1>
       <img
-        class="rounded-full w-20 grayscale lg:w-40"
+        class="rounded-full w-20 lg:hidden"
         src="../assets/myself.jpg"
         alt="Picture of myself"
       />
     </div>
-    <p class="pt-6 text-wrap text-lg lg:text-xl">
-      I'm a passionate software developer with an MBO degree in Software
-      Development and currently pursuing an Associate degree as a software
-      developer at Saxion University of Applied Sciences. I enjoy working
-      primarily in backend development, using languages like C# and Python.
-    </p>
-    <p class="pt-6 text-wrap text-lg lg:text-xl">
-      Recently, I’ve been diving into lower-level languages like C++ and Rust,
-      which has been both a challenging and rewarding journey. While these
-      languages demand a deeper understanding of memory management and
-      performance optimization, they provide valuable insights into how software
-      interacts with hardware. The learning curve is steep, but the satisfaction
-      of solving complex problems and gaining a more in-depth knowledge of
-      systems programming makes it a fun and exciting experience.
-    </p>
-    <p class="pt-6 text-wrap text-lg lg:text-xl">
-      This portfolio is built using Vue.js, HTML, and Tailwind CSS to showcase
-      some of my favorite projects. I'm always eager to learn and take on new
-      challenges in the ever-evolving world of software development.
-    </p>
+    <div class="flex items-center">
+      <p class="pt-6 text-lg lg:text-xl">
+        I'm a passionate software developer with an MBO degree in Software
+        Development and currently pursuing an Associate degree as a software
+        developer at Saxion University of Applied Sciences. I enjoy working
+        primarily in backend development, using languages like C# and Python.
+        <br /><br />
+        Recently, I’ve been diving into lower-level languages like C++ and Rust,
+        which has been both a challenging and rewarding journey. While these
+        languages demand a deeper understanding of memory management and
+        performance optimization, they provide valuable insights into how
+        software interacts with hardware. The learning curve is steep, but the
+        satisfaction of solving complex problems and gaining a more in-depth
+        knowledge of systems programming makes it a fun and exciting experience.
+        <br /><br />
+        This portfolio is built using Vue.js, HTML, and Tailwind CSS to showcase
+        some of my favorite projects. I'm always eager to learn and take on new
+        challenges in the ever-evolving world of software development.
+      </p>
+      <img
+        class="pl-3 rounded-full w-80 h-80 max-lg:hidden"
+        src="../assets/myself.jpg"
+        alt=""
+      />
+    </div>
   </div>
-  
+
   <h2 class="text-4xl font-bold text-center pt-12 pb-6">Showcase</h2>
   <h3 class="text-6xl font-bold text-center">T.B.C</h3>
 </template>

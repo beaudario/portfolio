b0d3b7e17e1563bfbcff6ead96e765ab75a96ff7
beaudario <dariodouma@icloud.com>
2024-11-06 13:18:11 +0100

    Small bug with the analytics manager that would destroy the router, but it's fixed

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "../router";
-import { config } from "vue-gtag";
+import VueGtagPlugin, { config } from "vue-gtag";
 
 createApp(App)
-  .use(VueGtag, { config: { id: "G-SDPQXV8DCZ" } }, router)
+  .use(VueGtagPlugin, { config: { id: "G-SDPQXV8DCZ" } })
+  .use(router)
   .mount("#app");

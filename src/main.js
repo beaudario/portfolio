e93dfa105dd5f1db6193e98e7f118efd47082916
beaudario <dariodouma@icloud.com>
2024-11-06 13:13:45 +0100

    Removed unused google tag manager and added a package for managing it

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '../router'
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "../router";
+import { config } from "vue-gtag";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(VueGtag, { config: { id: "G-SDPQXV8DCZ" } }, router)
+  .mount("#app");
